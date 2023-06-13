b74a253e275bd38effd3ad9684a73d79c624aabf
EnzoMagurno <magurnoenzo31602@gmail.com>
2023-06-13 11:04:26 -0300

    redux toolkit adedd to project

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+'use client'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counterSlice'
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+})
+
+export default store
